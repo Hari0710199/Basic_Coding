74198f13c3a5382f92a3788595338709dd1f1024
Hari0710199 <rajollakrishnareddy16@gmail.com>
2026-02-11 08:21:53 +0530

    Update LongestNumber.java with commit message easy way to understand

--- Example_helloworld/src/com/java/streams/LongestNumber.java
+++ Example_helloworld/src/com/java/streams/LongestNumber.java
@@ -6,14 +6,19 @@ import java.util.List;
 
 public class LongestNumber {
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(10, 45, 3, 99, 23);
+		// interface <generics> object = class.staticmethod(only accepts 10 to 15 input parameters);
+		List<Integer> list = Arrays.asList(10, 45, 3, 99, 23); 
 
+		//Datatype variableInstallization = Assignvaluesin collections.staticMethod(list);
 		int max = Collections.max(list);
 
+		//Class.staticvarible.staicMethod
 		System.out.println("Using max " + max);
 
+		//Datatype variableInstallization = int varibles
 		int[] arr = { 10, 45, 3, 99, 23 };
 
+		//Datatype variableInstallization = ArraysClass.streamStaticMethod.maxMethod.
 		int max1 = Arrays.stream(arr).max().orElseThrow();
 
 		System.out.println("Using Array " + max1);
